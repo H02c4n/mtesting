cf3db3e7e69cf8b67112245c2c113df87fce9a92
halil <svtr.hozcan@gmail.com>
2023-04-12 10:10:46 +0200

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+
+
+it('should render App component without crashing', ()=> {
+  render(<App/>)
+  const element = screen.getByText('Modern testing');
+
+  expect(element).toBeInTheDocument();
+})
